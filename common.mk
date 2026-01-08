6a1c192f9e6a65da78d79d6c32f9948b321f9655
Majaahh <maja.blaszczak@proton.me>
2026-01-08 14:57:26 +0100

    s5e8825: Namespace the tree
    Change-Id: I18ee53da43128fc6b1ce5779cfa70e21c450b106
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -2,3 +2,8 @@
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+COMMON_PATH := device/samsung/s5e8825-common
+
+# Soong - Namespaces
+PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
