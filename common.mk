a3142b80cf9c70eaf21886d4bb1a2f31a974ba9e
Majaahh <maja.blaszczak@proton.me>
2026-01-08 15:03:20 +0100

    s5e8825: Setup proprietary files
    Change-Id: I6e993edba5dcfbffce9cb21c537bd3ef661f3f6b
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -3,5 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit proprietary blobs
+$(call inherit-product, vendor/samsung/s5e8825-common/s5e8825-common-vendor.mk)
+
 # Soong - Namespaces
 PRODUCT_SOONG_NAMESPACES += device/samsung/s5e8825-common
