44762ec502f1a113fa1de7ad7e581a0c9d1b48b7
Majaahh <maja.blaszczak@proton.me>
2026-01-08 15:06:04 +0100

    s5e8825: Define architecture flags
    Change-Id: I01f32ff1fb7e162e9cd9a7cc8db1326990f12c94
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# All components inherited here go to system image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
 # Inherit proprietary blobs
 $(call inherit-product, vendor/samsung/s5e8825-common/s5e8825-common-vendor.mk)
 
