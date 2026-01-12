d969c21ff67c3a569fe29c8ebbf897dbd0a9670a
Majaahh <maja.blaszczak@proton.me>
2026-01-12 11:06:33 +0100

    s5e8825: Setup TEEgris
    Change-Id: Ib7cfa5607a6da50c1e40a7cc17ce8577c0eaa24f
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -4,14 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
+
+def lib_fixup_suffix(lib: str, *_):
+    return f'{lib}.samsung'
+
+
+lib_fixups: lib_fixups_user_type = {'libuuid': lib_fixup_suffix}
+
 module = ExtractUtilsModule(
     's5e8825-common',
     'samsung',
+    lib_fixups=lib_fixups,
 )
 
 if __name__ == '__main__':
