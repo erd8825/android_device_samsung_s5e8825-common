c4c56bf1d8cb92eb42661bfc13f5f81b75d235e9
Majaahh <maja.blaszczak@proton.me>
2026-02-19 02:22:12 +0100

    s5e8825: Setup Keymint
    * libcrypto shim was imported from https://github.com/LineageOS/android_device_samsung_gta4xls-common/tree/lineage-23.2/shims/libcrypto
    
    Change-Id: I4990e1db7d8e645e5dd8dfea01f615584b86d696
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -22,7 +22,10 @@ def lib_fixup_suffix(lib: str, *_):
     return f'{lib}.samsung'
 
 
-namespace_imports = ['hardware/samsung']
+namespace_imports = [
+    'device/samsung/s5e8825-common',
+    'hardware/samsung',
+]
 
 blob_fixups: blob_fixups_user_type = {
     # Audio (- Dependecies)
@@ -46,6 +49,29 @@ blob_fixups: blob_fixups_user_type = {
         # Before: [b.lt 0x00382144]
         # After: [nop]
         .sig_replace('bf 02 08 6b ab 01 00 54', 'bf 02 08 6b 1f 20 03 d5'),
+    # Security - Keymint
+    (
+        'vendor/bin/hw/android.hardware.security.keymint-service.samsung',
+        'vendor/lib64/lib_android_keymaster_skeymint_utils.so',
+        'vendor/lib64/libskeymint.so',
+        'vendor/lib64/libskeymint10device.so',
+        'vendor/lib64/libskeymint_cli.so',
+        'vendor/lib64/vendor.samsung.hardware.keymint-V1-ndk_platform.so',
+    ): blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so')
+        .add_needed('libbase_shim.so')
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
+        .replace_needed('lib_android_keymaster_keymint_utils.so', 'lib_android_keymaster_skeymint_utils.so')
+        .replace_needed('libcrypto.so', 'libcrypto-v33.so')
+        .replace_needed('libkeymint.so', 'libskeymint.so')
+        .replace_needed('libkeymaster_portable.so', 'libkeymaster_portable.samsung.so'),
+    'vendor/etc/init/android.hardware.security.keymint-service.samsung.rc': blob_fixup().regex_replace('-service', '-service.samsung'),
+    (
+        'vendor/lib64/libskeymint10device.so',
+        'vendor/lib64/libskeymint_cli.so',
+    ): blob_fixup().add_needed('libshim_crypto.so'),
 }  # fmt: skip
 
 lib_fixups: lib_fixups_user_type = {'libuuid': lib_fixup_suffix}
---
+++ shims/libhermes/hermes.c
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int hermes_power_off(void) {
+    return 10; // 6,"hermes_client","!@ %s(%d) cannot connect socket ret %d :%s(%d)", pcVar7,0x89,iVar2,pcVar5,*piVar3
+}
+
+int hermes_power_on(void) {
+    return 10; // 6,"hermes_client","!@ %s(%d) cannot connect socket ret %d :%s(%d)", pcVar7,0x89,iVar2,pcVar5,*piVar3
+}
