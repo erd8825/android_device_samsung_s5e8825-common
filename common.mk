dfc2e55b317bec8221431d6fdc186f856cd12c59
Majaahh <maja.blaszczak@proton.me>
2026-02-21 16:15:21 +0100

    s5e8825: Setup Neural Networks
    Change-Id: I89b0da99d77c31d12e9acc5a2c1578ba988735f7
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -138,6 +138,11 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 # Kernel - Modules
 PRODUCT_PACKAGES += toolbox.vendor_ramdisk
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3-service.eden-drv \
+    vendor.samsung_slsi.hardware.eden_runtime@1.0-service
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayCommon \
--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,23 @@ def rename_dynamic_symbol(
 
 
 blob_fixups: blob_fixups_user_type = {
+    # Neural Networks
+    'vendor/lib64/libeden_ud_gpu.so': blob_fixup()
+        .add_needed('libeden_ud_cpu.so')
+        .replace_needed('libOpenCL.so', 'libGLES_mali.so'),
+    'vendor/lib64/libgraphgen_ann_import_s.so': blob_fixup()
+         .clear_symbol_version('AHardwareBuffer_describe')
+         .clear_symbol_version('AHardwareBuffer_lock')
+         .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/libnpuc_backend.so': blob_fixup().add_needed('libnpuc_cmdq.so'),
+    'vendor/lib64/libnpuc_graph.so': blob_fixup().add_needed('libnpuc_common.so'),
+    (
+        'vendor/lib64/libnpuc_backend.so',
+        'vendor/lib64/libnpuc_common.so',
+        'vendor/lib64/libnpuc_controller.so',
+        'vendor/lib64/libnpuc_frontend.so',
+        'vendor/lib64/libnpuc_template.so'
+    ): blob_fixup().add_needed('liblog.so'),
     # RIL
     'vendor/etc/init/init.baseband.rc': blob_fixup().regex_replace('\n.*\n.*\n.*nic\n', ''),
     'vendor/etc/init/init.vendor.rilcommon.rc': blob_fixup().regex_replace('\n.*\n.*n}\n', ''),
