b670a8472c9b09a8083a55de5a8f5e731a761d1b
Majaahh <maja.blaszczak@proton.me>
2026-02-21 20:13:30 +0100

    s5e8825: Setup AOD
    Change-Id: Id5c5dfbd3660ca436b3646c8b4274b9cc0046ab2
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ products/aod/product.mk
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Doze
+PRODUCT_PACKAGES += SamsungDoze
+
+# Overlays
+PRODUCT_PACKAGES += FrameworkResOverlayAOD
