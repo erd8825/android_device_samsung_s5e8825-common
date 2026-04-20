598b1b9ee861f3e680ba1a0085ae397a0484e280
Secondarian <robertanitei@tutanota.com>
2026-04-20 16:09:40 +0200

    s5e8825: Inherit common makefiles
    Change-Id: I49cef9db7c670b841055f46efab5df78e152b603
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -5,11 +5,30 @@
 
 # All components inherited here go to system image
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system.mk)
+
+# All components inherited here go to system_ext image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+
+# All components inherited here go to product image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
+# All components inherited here go to vendor image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/media_vendor.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Inherit common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit proprietary blobs
 $(call inherit-product, vendor/samsung/s5e8825-common/s5e8825-common-vendor.mk)
 
 COMMON_PATH := device/samsung/s5e8825-common
+
+# Permissions
+PRODUCT_PACKAGES += handheld_core_hardware.prebuilt.xml
 
 # Soong - Namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
