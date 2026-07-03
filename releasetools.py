abb2a949bcfd10e0d209bae19f8878c2c45e96ed
Majaahh <maja.blaszczak@proton.me>
2026-07-03 20:26:51 +0200

    s5e8825: Update RIL to A556BXXSGDZG2
    * Due to RIL updates in One UI 8.0, we need to update modem to some newer one (at least One UI 8.0).
      Let's use modem from first ever released One UI 8.0 to avoid bumping user's RP.
    
    Change-Id: If7140e8fda88ee04fb1341934919695265dbf613
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- releasetools.py
+++ releasetools.py
@@ -5,6 +5,7 @@
 #
 
 import common
+import re
 
 
 def FullOTA_InstallEnd(info):
@@ -16,8 +17,8 @@ def IncrementalOTA_InstallEnd(info):
     OTA_InstallEnd(info)
 
 
-def AddImage(info, basename, dest):
-    data = info.input_zip.read('IMAGES/' + basename)
+def AddImage(info, basename, dest, dir='IMAGES'):
+    data = info.input_zip.read(dir + '/' + basename)
     common.ZipWriteStr(info.output_zip, basename, data)
     image = format(dest.split('/')[-1])
     info.script.Print(f'Patching {image} image unconditionally...')
@@ -28,3 +29,18 @@ def OTA_InstallEnd(info):
     AddImage(info, 'dtbo.img', '/dev/block/by-name/dtbo')
     AddImage(info, 'vbmeta.img', '/dev/block/by-name/vbmeta')
     AddImage(info, 'vendor_boot.img', '/dev/block/by-name/vendor_boot')
+
+    for e in info.input_zip.namelist():
+        match = re.match(r'^RADIO/modem\.bin(?:_(.+))?$', e)
+        if not match:
+            continue
+
+        model = match.group(1)
+        if model is None:
+            AddImage(info, 'modem.bin', '/dev/block/by-name/radio', 'RADIO')
+        else:
+            # fmt: off
+            info.script.AppendExtra(f'if getprop("ro.boot.em.model") == "{model}" then')
+            AddImage(info, f'modem.bin_{model}', '/dev/block/by-name/radio', 'RADIO')
+            # fmt: on
+            info.script.AppendExtra('endif;')
