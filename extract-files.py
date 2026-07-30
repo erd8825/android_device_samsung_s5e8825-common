2f0452d60c7011381e346e0c204521bdf52202e5
Majaahh <maja.blaszczak@proton.me>
2026-07-31 01:21:05 +0200

    s5e8825: shims: Minor adjustments
    * Beautify blueprints
    * Cleanup
    * Reformat with clang
    * Rename some shims
    * Switch to SPDX license headers and drop dates
    
    Change-Id: I7022a6d617404361626b0203efc6f0f93e3ec28c
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -52,7 +52,7 @@ def rename_dynamic_symbol(
 
 blob_fixups: blob_fixups_user_type = {
     # Camera - Dependecies
-    'vendor/lib64/libsensorlistener.so': blob_fixup().add_needed('libshim_sensorndkbridge.so'),
+    'vendor/lib64/libsensorlistener.so': blob_fixup().add_needed('libsensorndkbridge_shim.samsung.so'),
     # Neural Networks
     'vendor/lib64/libeden_ud_gpu.so': blob_fixup()
         .add_needed('libeden_ud_cpu.so')
--- shims/libepicoperator/epicoperator.c
+++ shims/libepicoperator/epicoperator.c
@@ -1,8 +1,9 @@
-unsigned int createOperator(int param_1 __attribute__((unused))) {
-    return 0;
-}
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 unsigned int createScenarioOperator(int param_1 __attribute__((unused)),
-        int param_2 __attribute__((unused))) {
+                                    int param_2 __attribute__((unused))) {
     return 0;
 }
