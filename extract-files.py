fb13427bced20e1b11877f74f71498f8a6248b55
Secondarian <robertanitei@tutanota.com>
2026-08-12 09:36:00 +0300

    s5e8825: Shim BnSehKeyMintFactory and BnSehKeyMintExtension
    * One UI only interface.
    
    Change-Id: Ib121322aeef29d382f868804ca6b7bcac392097d
    Signed-off-by: ExtremeXT <extremext@lineageos.org>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -107,9 +107,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
-    'vendor/lib64/vendor.samsung.hardware.keymint-V1-ndk_platform.so': blob_fixup()
-        .add_needed('android.hardware.security.rkp-V1-ndk.so')
-        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so'),
     # Sensors - Dependecies
     (
         'vendor/lib64/sensors.grip.so',
