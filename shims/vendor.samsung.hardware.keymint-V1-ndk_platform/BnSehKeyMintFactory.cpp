71d20bda7c5583e28c93e1763a73dee17e9717d5
Secondarian <robertanitei@tutanota.com>
2026-08-12 09:36:00 +0300

    s5e8825: Shim BnSehKeyMintFactory and BnSehKeyMintExtension
    * One UI only interface.
    
    Change-Id: Ib121322aeef29d382f868804ca6b7bcac392097d
    Signed-off-by: ExtremeXT <extremext@lineageos.org>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ shims/vendor.samsung.hardware.keymint-V1-ndk_platform/BnSehKeyMintFactory.cpp
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string>
+
+namespace aidl::vendor::samsung::hardware::keymint {
+using namespace std;
+
+class BnSehKeyMintFactory {
+    BnSehKeyMintFactory();
+    ~BnSehKeyMintFactory();
+    void createBinder();
+    void getInterfaceHash(basic_string<char, char_traits<char>, allocator<char>>*);
+    void getInterfaceVersion(int*);
+};
+
+BnSehKeyMintFactory::BnSehKeyMintFactory() {}
+
+BnSehKeyMintFactory::~BnSehKeyMintFactory() {}
+
+void BnSehKeyMintFactory::createBinder() {}
+
+void BnSehKeyMintFactory::getInterfaceHash(
+        basic_string<char, char_traits<char>, allocator<char>>*) {}
+
+void BnSehKeyMintFactory::getInterfaceVersion(int*) {}
+}  // namespace aidl::vendor::samsung::hardware::keymint
