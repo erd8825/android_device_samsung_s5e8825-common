24126614b1c28d9986a1cabf03cbbf22c990e0a7
Majaahh <maja.blaszczak@proton.me>
2026-09-09 16:56:49 +0200

    s5e8825: Inherit common Lineage stuff
    Change-Id: If96a086453bf9f7d07cf178386c86e10fde8c02a
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ products/phone/product.mk
@@ -0,0 +1,7 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
